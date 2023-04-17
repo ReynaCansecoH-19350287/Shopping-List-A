c38c27b45fa82c6e7d1e82c1d5bda96073ce4ab4
ReynaCansecoHernandez <l19350287@tuxtepec.tecnm.mx>
2023-04-17 10:06:04 -0600

    Modifica la App.jsx para la Agregación de la Funcion ItemChecked en Shopping-List

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
+import { useState } from "react"
+
 function App() {
+  const [itemChecked, setItemChecked] = useState({
+    first: false,
+    second: false,
+    third: false,
+  })
+
+  const handleItemChecked = (e) => {
+    /*if (itemChecked[e.target.name]) {
+      setItemChecked({
+        ...itemChecked,
+        [e.target.name]: false,
+      });
+    } else {
+      setItemChecked({
+        ...itemChecked,
+        [e.target.name]: true,
+      });
+    }*/
+    setItemChecked({
+      ...itemChecked,
+      [e.target.name]: !itemChecked[e.target.name]
+    })
+  }
 
   return (
     <div className="container text-center">
@@ -15,10 +40,29 @@ function App() {
       <hr />
       <div className="row">
         <div className="col">
-          <input type="checkbox" />
+          <input
+          checked={itemChecked.first}
+          name="first"
+          onClick={(e) => handleItemChecked(e)}
+          type="checkbox" 
+          />
         </div>
-        <div className="col-2 text-start">1 Kg</div>
-        <div className="col-5 col-md-6 text-start">Tortillas</div>
+        <div className="col-2 text-start">
+          {
+            //itemChecked.first && <del>1 Kg</del>
+          }
+          {
+            //itemChecked.first && "1 Kg"
+          }
+          {
+            itemChecked.first ? <del>1 Kg</del>: "1 Kg"
+          }
+          </div>
+        <div 
+        className="col-5 col-md-6 text-start" 
+        style={{textDecoration: itemChecked.first && "Line-through"}}>
+          Tortillas
+          </div>
         <div className="col-4 col-md-3 btn-group btn-group-sm" role="group">
           <button type="button" className="btn btn-outline-primary">
             <i className="bi bi-pencil-square"></i>
