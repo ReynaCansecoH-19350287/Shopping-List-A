8d057712a6a37d14307cb70e461598b8674f7c7c
ReynaCansecoHernandez <l19350287@tuxtepec.tecnm.mx>
2023-04-25 10:10:08 -0600

    Agregacion de la Funcion Alert en NewItemButton.jsx en Shopping-List

--- src/components/NewItemButton.jsx
+++ src/components/NewItemButton.jsx
@@ -1,10 +1,56 @@
 import Swal from "sweetalert2"
 
-const NewItemButton = () => {
-    const  newItemModal = () => {
-         
-Swal.fire('Any fool can use a computer')
+const NewItemButton = ({listItems, setListItems}) => {
+    const  newItemModal = async () => {
+      const {value} = await Swal.fire({
+        title: "New item Information",
+        html: `
+        <input 
+        class="swal2-input"
+        id="name" 
+        name="name"
+        placeholder="Name" 
+        type="text" 
+        />
+        <input 
+        class="swal2-input"
+        id="quantity" 
+        name="quantity"
+        placeholder="Quantity" 
+        type="numeric" 
+        />
+        <input 
+        class="swal2-input"
+        id="unit" 
+        name="unit"
+        placeholder="Unit" 
+        type="text" 
+        />
+        `,
+        confirmButtonText: "Add item",
+        showCancelButton: true,
+        cancelButtonText: "Dismiss",
+        focusConfirm: false,
+        preConfirm: () => {
+          const name = Swal.getPopup().querySelector("#name").value;
+          const quantity = Swal.getPopup().querySelector("#quantity").value;
+          const unit = Swal.getPopup().querySelector("#unit").value;
+          if(!name || !quantity || !unit) {
+            Swal.showValidationMessage("Please enter a name");
+          }
+          return {name, quantity, unit};
+        }
+      })
+      setListItems([
+        ...listItems,
+        {
+          id: listItems.length + 1,
+          ...value,
+          checked: false,
+        }
+      ])     
     }
+
   return (
     <button 
     className="btn btn-outline-primary"
