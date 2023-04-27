b35fbd7179c9f148c9b6f414857509caf50facde
ReynaCansecoHernandez <l19350287@tuxtepec.tecnm.mx>
2023-04-27 10:23:21 -0600

    Agregacion SweetAlert (Mensaje de lista vacia) en Shopping-List

--- src/App.jsx
+++ src/App.jsx
@@ -61,14 +61,20 @@ function App() {
       </div>
       <hr />
       {
+        listItems.length === 0 && (
+          <div>
+            <h3>Your list is empty...</h3>
+            Please add a new item to start.
+          </div>
+        )
+      }
+      {
         listItems.map((item) => (
           <ListItem 
-          id={item.id}
-          name={item.name}
-          quantity={item.quantity}
-          unit={item.unit}
-          checked={item.checked}
+          item={item}
           handleItemChecked={handleItemChecked}
+          listItems={listItems}
+          setListItems={setListItems}
           />
         ))
       }
